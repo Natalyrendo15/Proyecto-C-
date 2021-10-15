6270751d12b075420469a9083964cc0d477d1156
Natalyrendo15 <92136037+Natalyrendo15@users.noreply.github.com>
2021-10-14 21:55:14 -0500

    Create numero del 1 al 100.cpp
    se usa la estructura del do y while

---
+++ numero del 1 al 100.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int numero;
+	do{
+		cout<<"ingresa un numero menor a 100 si quieres regresar y uno mayor a 100 si quieres terminar"<<endl;
+		cin>>numero;
+	
+	}
+	while(numero <=100);
+	
+return 0;
+cin.get();
+
+
+
+}
