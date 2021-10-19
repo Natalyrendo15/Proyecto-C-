05103cce2f694c3beb05eb4bbd5bf05f1ab04241
Natalyrendo15 <92136037+Natalyrendo15@users.noreply.github.com>
2021-10-19 17:54:00 -0500

    Create desimal a binario.cpp
    se realizo la practica de conversión de decimal a binario 19/10/21

---
+++ desimal a binario.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main(){
+	int z, x,numero, n2=0;
+	int binario[50];
+	binario[0]=0;
+	cout<<"Bienvenido al converrsor de decimal a binario \n";
+	cout<<"Dijita la decimal a combertir \n";
+	cin>>numero;
+	for(x=0; x<50; x++){
+	binario  [x]     = numero%2;
+	numero = numero/2;
+	if(numero == 0)
+	break;
+	
+	}
+	for(z=x; z>0; z--){
+	cout<<binario [z];
+	}
+
+return 0;
+cin.get();
